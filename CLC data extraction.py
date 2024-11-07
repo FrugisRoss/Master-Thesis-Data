d1936e3774bf0ad52a2cd4d3bd1c82024d0c2740
FrugisRoss <167289528+FrugisRoss@users.noreply.github.com>
2024-11-07 16:41:18 +0100

    Biodiversity shapefile filetered and merged

--- CLC data extraction.py
+++ CLC data extraction.py
@@ -127,3 +127,8 @@ CLC_water_bodies_gdf = filter_merge_save(CLC_gdf, 'Code_18', '5', water_bodies_f
 # Potected areas from Biodiversity Council
 
 Biodiversity_30_gdf = gpd.read_file(r'C:\Users\Utente\OneDrive - Politecnico di Milano\polimi\magistrale\DTU\Input Data\QGIS data\Biodiversity Council\scenarie_030_vector.shp')
+
+Bio30_path =r'C:\Users\Utente\OneDrive - Politecnico di Milano\polimi\magistrale\DTU\Input Data\QGIS data\Biodiversity Council\Biodiversity30.shp'
+Bio30_merged_path = r'C:\Users\Utente\OneDrive - Politecnico di Milano\polimi\magistrale\DTU\Input Data\QGIS data\Biodiversity Council\Biodiversity30_merged.shp'
+
+Bio30_merged_gdf = filter_merge_save(Biodiversity30_gdf, 'DN', '1', Bio30_path, Bio30_merged_path)
